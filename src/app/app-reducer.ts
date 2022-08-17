bcb34abb6e0e39948b5ea40df7de9f93baf8df6b
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-17 18:34:18 +0300

    feat: add app reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,26 +1,43 @@
 import {Dispatch} from "redux";
 import {authAPI} from "../api/todolists-api";
-import {setInLogged} from "../features/Login/authReducer";
+import {authSlice, setInLogged} from "../features/Login/authReducer";
 import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
-
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 const initialState: InitialStateType = {
   status: 'idle',
   error: null,
   isInitialized: false,
 }
-
-export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-  switch (action.type) {
-    case 'APP/SET-STATUS':
-      return {...state, status: action.status}
-    case 'APP/SET-ERROR':
-      return {...state, error: action.error}
-    case "APP/SET-INITIALIZED":
-      return {...state, isInitialized: action.value}
-    default:
-      return {...state}
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: initialState,
+  reducers: {
+    setAppStatus(state, action: PayloadAction<{status: RequestStatusType}>){
+      state.status = action.payload.status
+    },
+    setAppError(state, action: PayloadAction<{error: string | null}>){
+      state.error = action.payload.error
+    },
+    setInitialized(state, action: PayloadAction<{value: boolean}>){
+      state.isInitialized = action.payload.value
+    }
   }
-}
+})
+export const {setAppStatus, setAppError, setInitialized} = appSlice.actions
+export const appReducer = appSlice.reducer
+
+// export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+//   switch (action.type) {
+//     case 'APP/SET-STATUS':
+//       return {...state, status: action.status}
+//     case 'APP/SET-ERROR':
+//       return {...state, error: action.error}
+//     case "APP/SET-INITIALIZED":
+//       return {...state, isInitialized: action.value}
+//     default:
+//       return {...state}
+//   }
+// }
 export const initializeAppTC = () => (dispatch: Dispatch) => {
   authAPI.me()
     .then(res => {
@@ -31,7 +48,7 @@ export const initializeAppTC = () => (dispatch: Dispatch) => {
       }
     })
     .catch(err => handleServerNetworkError(err, dispatch))
-    .finally(() => dispatch(setInitializedAC(true)))
+    .finally(() => dispatch(setInitialized({value: true})))
 }
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
@@ -43,15 +60,15 @@ export type InitialStateType = {
   isInitialized: boolean
 }
 
-export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
-export const setInitializedAC = (value: boolean) => ({type: 'APP/SET-INITIALIZED', value} as const)
+// export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+// export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+// export const setInitializedAC = (value: boolean) => ({type: 'APP/SET-INITIALIZED', value} as const)
 
-export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
-export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
-export type SetInitializedAT = ReturnType<typeof setInitializedAC>
-
-type ActionsType =
-  | SetAppErrorActionType
-  | SetAppStatusActionType
-  | SetInitializedAT
+// export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+// export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+// export type SetInitializedAT = ReturnType<typeof setInitializedAC>
+//
+// type ActionsType =
+//   | SetAppErrorActionType
+//   | SetAppStatusActionType
+//   | SetInitializedAT
--- src/features/Login/authReducer.ts
+++ src/features/Login/authReducer.ts
@@ -3,6 +3,7 @@ import {Dispatch} from "redux";
 import {authAPI} from "../../api/todolists-api";
 import {FormikErrorType} from "./Login";
 import {handleServerAppError, handleServerNetworkError} from "../../utils/error-utils";
+import {setAppStatus} from "../../app/app-reducer";
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -18,10 +19,12 @@ export const authSlice = createSlice({
 export const {setInLogged} = authSlice.actions
 export const authReducer = authSlice.reducer
 export const loginTC = (data: FormikErrorType) => (dispatch: Dispatch<any>) => {
+  dispatch(setAppStatus({status: 'loading'}))
   authAPI.login(data)
     .then(res => {
       if (res.data.resultCode === 0) {
         dispatch(setInLogged({value: true}))
+        dispatch(setAppStatus({status: 'succeeded'}))
       } else {
         handleServerAppError(res.data, dispatch)
       }
