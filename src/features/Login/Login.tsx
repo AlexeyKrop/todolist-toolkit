fa63b6d434e9cb1354426d5b8998723171f69897
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-17 11:48:10 +0300

    feat: add validate

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -28,6 +28,11 @@ export const Login = () => {
       } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address'
       }
+      if(!values.password){
+        errors.email = 'Required'
+      }else if(values.password.length <= 3){
+        errors.password = 'Invalid password'
+      }
       return errors
     },
     onSubmit: values => {
