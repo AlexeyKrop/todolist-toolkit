933f2ce816dc5a2f50696a8d4ee26e34e55b7089
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-17 09:22:27 +0300

    feat: add Login component

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': 'e740762a-947d-4fd0-b06d-9b4eb349c99e'
     }
 })
 
