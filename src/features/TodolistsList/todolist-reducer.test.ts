a43a63e8edd9b921b684acecb1b222201bbbb853
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-18 08:35:25 +0300

    feat: add check add todolist

--- src/features/TodolistsList/todolist-reducer.test.ts
+++ src/features/TodolistsList/todolist-reducer.test.ts
@@ -1,6 +1,28 @@
-import {removeTodolistAC, TodolistDomainType, todolistsReducer} from "./todolists-reducer";
+import {addTodolistAC, removeTodolistAC, TodolistDomainType, todolistsReducer} from "./todolists-reducer";
 
 test('check remove todolist', () => {
+  const startState:  Array<TodolistDomainType> = [
+    {
+      id: '1',
+      title: 'js',
+      addedDate: 'string',
+      order: 1,
+      filter: 'all',
+      entityStatus: 'idle',
+    },
+    {
+      id: '2',
+      title: 'ts',
+      addedDate: 'string',
+      order: 1,
+      filter: 'all',
+      entityStatus: 'idle',
+    }
+  ];
+  const endState = todolistsReducer(startState, removeTodolistAC('2'))
+  expect(endState.length).toBe(1)
+});
+test('check add todolist', () => {
   const startState:  Array<TodolistDomainType> = [
     {
       id: '1',
@@ -19,7 +41,14 @@ test('check remove todolist', () => {
       entityStatus: 'idle',
     }
   ];
-  const endState = todolistsReducer(startState, removeTodolistAC('2'))
-  expect(endState.length).toBe(1)
-
+  const todolist =  {
+    id: '3',
+    title: 'react',
+    addedDate: 'string',
+    order: 1,
+    filter: 'all',
+    entityStatus: 'idle',
+  }
+  const endState = todolistsReducer(startState, addTodolistAC(todolist))
+  expect(endState[0].title).toBe('react')
 });
